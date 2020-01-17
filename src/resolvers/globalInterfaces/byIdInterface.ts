7dfd5b31a7484198d52a8f0793d672aa75adf88c
victor stein <stein.hakase.vs@gmail.com>
2020-01-16 19:16:04 -0600

    removed deletebyid interface in favor of byidinterface

--- src/resolvers/globalInterfaces/byIdInterface.ts
+++ src/resolvers/globalInterfaces/byIdInterface.ts
@@ -2,7 +2,7 @@ import { ArgsType, Field } from "type-graphql";
 import { IsMongoId } from "class-validator";
 
 @ArgsType()
-export default class deleteOne {
+export default class byIdInterface {
   @Field({ nullable: false })
   @IsMongoId({ message: 'The provided ID is invalid' })
   id: string

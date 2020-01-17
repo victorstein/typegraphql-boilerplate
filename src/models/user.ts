91e043a47784b9ace181d84711db69b03c35f9cd
victor stein <stein.hakase.vs@gmail.com>
2020-01-16 19:15:09 -0600

    created pre script to hash the password before saving the user and adding roles automatically

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,31 @@
 import { ObjectType } from "type-graphql";
-import { prop, modelOptions, getModelForClass, Ref, arrayProp } from '@typegoose/typegoose'
+import { prop, modelOptions, getModelForClass, Ref, arrayProp, pre } from '@typegoose/typegoose'
 import { Field, ID } from 'type-graphql'
 import { Role, roleModel } from "./role";
 import { Permission } from "./permission";
+import bcrypt from 'bcryptjs'
+import mongoose from "mongoose";
+
+@pre<User>('save', async function (next): Promise<void> {
+  // Hash the password before saving
+  this.password = await bcrypt.hash(this.password, 12)
+  
+  // Get the admin and base role
+  const adminRole = await roleModel.findOne({ usedFor: 'adminRole' }, { id: 1 })
+  const baseRole = await roleModel.findOne({ usedFor: 'baseRole' }, { id: 1 })
+
+  // Check that these roles exists
+  if (!adminRole || !baseRole) { throw new Error('Server Error') }
+
+  // Assign the admin role if is the first user on the DB
+  if (await userModel.estimatedDocumentCount() > 0) {
+    this.role = mongoose.Types.ObjectId(baseRole._id)
+  } else {
+    this.role = mongoose.Types.ObjectId(adminRole._id)
+  }
+
+  next()
+})
 
 @ObjectType()
 @modelOptions({ schemaOptions: { timestamps: true } })
@@ -10,30 +33,23 @@ export class User {
   @Field(() => ID)
   id: string
 
-  @prop()
-  @Field()
+  @prop({ required: true })
+  @Field({ nullable: false })
   firstName: string
 
-  @prop()
-  @Field()
+  @prop({ required: true })
+  @Field({ nullable: false })
   lastName: string
 
-  @prop({ lowercase: true })
-  @Field()
+  @prop({ lowercase: true, required: true })
+  @Field({ nullable: false })
   email: string
 
-  @prop()
+  @prop({ required: true })
   password: string
 
   @prop({ ref: Role })
-  @Field(() => Role, {defaultValue: async () => {
-    // Get the admin and base role
-    const roles = await roleModel.find({ usedFor: { '$in': ['adminRole', 'baseRole'] } }, { id: 1 })
-    console.log(roles)
-    if (await userModel.estimatedDocumentCount() > 0) {
-
-    }
-  }})
+  @Field(() => Role, { nullable: false })
   role: Ref<Role>
 
   @arrayProp({ itemsRef: 'Permission' })

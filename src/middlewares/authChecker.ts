6f4f345c314f960f241e58187fdd5ee32ef5a20e
steinhakasevs <stein.hakase.vs@gmail.com>
2020-01-19 23:53:37 -0600

    Configured authChecker to handle permissions

--- src/middlewares/authChecker.ts
+++ src/middlewares/authChecker.ts
@@ -3,8 +3,9 @@ import { AuthenticationError } from "apollo-server-express"
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 import { userModel } from "../models/user"
+import { permissionModel } from "../models/permission"
 
-const authChecker:AuthChecker<any> = async ({ context }): Promise<boolean> => {
+const authChecker:AuthChecker<any> = async ({ context }, permissions): Promise<boolean> => {
   try {
     // Get headers from context
     let headers = context.req.headers
@@ -24,13 +25,31 @@ const authChecker:AuthChecker<any> = async ({ context }): Promise<boolean> => {
     // Return and error if no user found
     if (!user) { throw new Error('Insufficient permissions to perform this action') }
 
+    // Stablish default user permissions
+    let userPermissions: string[] = []
+
     // Check if there is any role requirement
-    /* if(roles.length) {
-      if (!roles.includes(user.role)) { throw new Error() }
-    } */
+    if(permissions.length) {
+      // Get all the paremissions associated to the role and any additional permissions
+      let allPermissions = await permissionModel.find({
+        $or: [
+          { usedByRole: user.role },
+          { _id: { $in: user.permissions } }
+        ]
+      }, { _id: 0, name: 1 })
+      
+      // transform the result in to a array of strings
+      userPermissions = allPermissions.map((u: any) => u.name)
+
+      // Check if the required permission exits in the user permissions
+      if (!permissions.some((u: string) => userPermissions.includes(u))) {
+        throw new Error('Insufficient permissions for this query')
+      }      
+    }
 
     // Add the user to context
     context.user = user
+    context.permissions = userPermissions
     return true
   } catch (e) {
     console.log(e.message)

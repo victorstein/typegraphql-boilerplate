f2090af388e8b9c923d9e6b4d7ccc8ea2582448d
steinhakasevs <stein.hakase.vs@gmail.com>
2020-01-19 23:54:20 -0600

    Removed permissions field Added description field Removed unnecesary console log

--- src/models/role.ts
+++ src/models/role.ts
@@ -1,12 +1,10 @@
 import { ObjectType, Field, ID } from "type-graphql";
-import { modelOptions, prop, arrayProp, getModelForClass, Ref, pre } from "@typegoose/typegoose";
-import { Permission } from "./permission";
+import { modelOptions, prop, getModelForClass, pre } from "@typegoose/typegoose";
 
 // ENSURE THAT WE ARE NOT DELETEING BASE ROLES
 @pre<Role>('remove', function(next) {
   // Check if this is one of the base roles
   if (this.usedFor) {
-    console.log('happening')
     next(new Error('Unable to delete the requested role'))
   }
 
@@ -23,13 +21,13 @@ export class Role {
   @Field({ nullable: false })
   name: string
 
+  @prop({ required: false })
+  @Field({ nullable: true })
+  description: string
+
   @prop({ required: false, enum: [undefined, 'adminRole', 'baseRole'] })
   @Field({ nullable: true })
   usedFor: string
-
-  @arrayProp({ itemsRef: 'Permission' })
-  @Field(() => [Permission])
-  permissions: Ref<Permission>[]
 }
 
 export const roleModel = getModelForClass(Role)

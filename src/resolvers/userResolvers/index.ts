fdf0abeeb9c1ac9f0aa715a198172fed47067e10
steinhakasevs <stein.hakase.vs@gmail.com>
2020-01-19 23:56:41 -0600

    Removed hello query, created create admin query, added authorization directive, and created login, me and fullName resolvers

--- src/resolvers/userResolvers/index.ts
+++ src/resolvers/userResolvers/index.ts
@@ -1,16 +1,23 @@
-import { Resolver, Mutation, Args, Query } from "type-graphql";
+import { Resolver, Mutation, Args, Query, Authorized, Ctx, FieldResolver, Root } from "type-graphql";
 import { User, userModel } from "../../models/user";
 import createUserInterface from "./interfaces/createUser";
-import { ApolloError } from "apollo-server-express";
+import { ApolloError, AuthenticationError } from "apollo-server-express";
+import Token from "./outputTypes/token";
+import loginInterface from "./interfaces/loginInterface";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+const {
+  TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+  TOKEN_SECRET_EXPIRATION,
+  REFRESH_TOKEN_SECRET_EXPIRATION
+} = process.env
 
 @Resolver(() => User)
 export default class userResolvers {
-  @Query(() => String)
-  hello () {
-    return 'hello world'
-  }
-
   @Mutation(() => User)
+  @Authorized('createUsers')
   async createUser(
     @Args() { email, password, confirmPassword, firstName, lastName }: createUserInterface
   ): Promise<User> {
@@ -34,8 +41,94 @@ export default class userResolvers {
         email
       })
     } catch (e) {
-      console.log(e)
       throw new ApolloError(e)
     }
+  }
+
+  @Mutation(() => User)
+  async createAdmin (
+    @Args() { email, password, confirmPassword, firstName, lastName }: createUserInterface
+  ) {
+    try {
+      // Check if the admin role was already created
+      if (await userModel.estimatedDocumentCount() > 0) {
+        throw new Error('Unable to process your request')
+      }
+
+      // Check if the user exists in the DB
+      const user = await userModel.findOne({ email })
+
+      // Throw error if user already exists
+      if (user) { throw new Error('Unable to process your request with the provided email') }
+
+      // if user does not exist proceed to chech that the passwords match
+      if (password !== confirmPassword) {
+        throw new Error('The passwords do not match')
+      }
+
+      // If the passwords match proceed to create the user
+      return userModel.create({
+        firstName,
+        lastName,
+        password,
+        email
+      })
+    } catch (e) {
+      throw new ApolloError(e)
+    }
+  }
+
+  @Query(() => Token)
+  async login (
+    @Args() { email, password }: loginInterface
+  ): Promise<Token> {
+    try {
+      // Check if the user exists on the Db
+      const user = await userModel.findOne({ email })
+
+      // Throw error if user doesnt exists
+      if (!user) { throw new Error('The provided email or password is invalid') }
+
+      // Check if the password matches the hash
+      const valid = await bcrypt.compare(password, user.password)
+
+      // If invalid return error
+      if (!valid) { throw new Error('The provided email or password is invalid') }
+
+      // If the login is valid proceed to check if the user has been verified
+      if (!user.verified) { throw new Error('The account has not yet been verified. Please check your email and verify your account.') }
+
+      // Create the payload
+      const payload = {
+        id: user._id,
+        role: user.role,
+        permissions: user.permissions,
+        version: user.tokenVersion
+      }
+
+      // If the account is verified then proceed to create the hashes
+      const token = jwt.sign(payload, TOKEN_SECRET!, { expiresIn: TOKEN_SECRET_EXPIRATION! })
+      const refreshToken = jwt.sign(payload, REFRESH_TOKEN_SECRET!, { expiresIn: REFRESH_TOKEN_SECRET_EXPIRATION! })
+
+      return { token, refreshToken }
+    } catch (e) {
+      throw new AuthenticationError(e)
+    }
+  }
+
+  @Query(() => User)
+  @Authorized()
+  me (
+    @Ctx() { user }: any
+  ): User {
+    // Return the user collected from the context
+    return user
+  }
+
+  @FieldResolver(() => String)
+  fullName (
+    @Root() root:any
+  ): string {
+    return `${root.firstName} ${root.lastName}`
   }
 }

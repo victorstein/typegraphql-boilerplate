911d6384c20409bb8e7195637ff1373fe3078bea
victor stein <stein.hakase.vs@gmail.com>
2020-01-21 16:38:57 -0600

    Updated the pre and psot hooks to work only if the item is new, also added wasNew to the user class to handle post hook limitation, finally added the passwordRecoveryVersion to invalidate the password reset link

--- src/models/user.ts
+++ src/models/user.ts
@@ -11,45 +11,52 @@ import jwt from 'jsonwebtoken'
 const { GLOBAL_SECRET, EMAIL_VERIFICATION_EXPIRY } = process.env
 
 @pre<User>('save', async function (next): Promise<void> {
-  // Hash the password before saving
-  this.password = await bcrypt.hash(this.password, 12)
-  
-  // Get the admin and base role
-  const adminRole = await roleModel.findOne({ usedFor: 'adminRole' }, { id: 1 })
-  const baseRole = await roleModel.findOne({ usedFor: 'baseRole' }, { id: 1 })
+  if (this.isNew) {
+    // Keep the is new data for post hook
+    this.wasNew = this.isNew
 
-  // Check that these roles exists
-  if (!adminRole || !baseRole) { throw new Error('Server Error') }
+    // Hash the password before saving
+    this.password = await bcrypt.hash(this.password, 12)
+    
+    // Get the admin and base role
+    const adminRole = await roleModel.findOne({ usedFor: 'adminRole' }, { id: 1 })
+    const baseRole = await roleModel.findOne({ usedFor: 'baseRole' }, { id: 1 })
 
-  // Assign the admin role if is the first user on the DB
-  if (await userModel.estimatedDocumentCount() > 0) {
-    this.role = mongoose.Types.ObjectId(baseRole._id)
-  } else {
-    this.role = mongoose.Types.ObjectId(adminRole._id)
+    // Check that these roles exists
+    if (!adminRole || !baseRole) { throw new Error('Server Error') }
+
+    // Assign the admin role if is the first user on the DB
+    if (await userModel.estimatedDocumentCount() > 0) {
+      this.role = mongoose.Types.ObjectId(baseRole._id)
+    } else {
+      this.role = mongoose.Types.ObjectId(adminRole._id)
+    }
   }
 
   next()
 })
 
-@post<User>('save', async function ({ _id, firstName, lastName, email }, next) {
+@post<User>('save', async function ({ _id, firstName, lastName, email, wasNew }, next): Promise<void> {
   try {
-    // Create a hash to auth the user
-    const hash = jwt.sign({ id: _id }, GLOBAL_SECRET!, { expiresIn: EMAIL_VERIFICATION_EXPIRY })
+    if (wasNew) {
+      // Create a hash to auth the user
+      const hash = jwt.sign({ id: _id }, GLOBAL_SECRET!, { expiresIn: EMAIL_VERIFICATION_EXPIRY })
 
-    // Once the new user is created proceed to send welcome email
-    const emailProvider = new EmailProvider({
-      to: email,
-      subject: 'Verify your account',
-      template: "welcome_email",
-      data: {
-        firstName: firstName,
-        lastName: lastName,
-        hash
-      }
-    })
+      // Once the new user is created proceed to send welcome email
+      const emailProvider = new EmailProvider({
+        to: email,
+        subject: 'Verify your account',
+        template: "welcome_email",
+        data: {
+          firstName: firstName,
+          lastName: lastName,
+          hash
+        }
+      })
 
-    // Send the email
-    await emailProvider.sendEmail()
+      // Send the email
+      await emailProvider.sendEmail()
+    }
 
     next!()
   } catch(e) {
@@ -63,6 +70,9 @@ const { GLOBAL_SECRET, EMAIL_VERIFICATION_EXPIRY } = process.env
 export class User {
   @Field(() => ID)
   id: string
+
+  // Used to overcome limitiations in post hook https://github.com/Automattic/mongoose/issues/2162
+  wasNew: boolean
 
   @prop({ required: true })
   @Field({ nullable: false })
@@ -89,6 +99,9 @@ export class User {
 
   @prop({ default: 1 })
   tokenVersion: number
+
+  @prop({ default: 1 })
+  passwordRecoveryVersion: number
 
   @prop({ default: false })
   @Field()

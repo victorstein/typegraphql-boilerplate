c1184e159cd155e33efdfd1ad8bfa81e63df44b5
victor stein <stein.hakase.vs@gmail.com>
2020-01-24 15:01:49 -0600

    Added refreshtoken type to add extra data to login service (token creation and expiration)

--- src/resolvers/userResolvers/outputTypes/token.ts
+++ src/resolvers/userResolvers/outputTypes/token.ts
@@ -1,9 +1,21 @@
 import { ObjectType, Field } from "type-graphql";
 
 @ObjectType()
+class refreshToken {
+  @Field({ nullable: false })
+  createdAt: number
+
+  @Field({ nullable: false })
+  expiresAt: number
+}
+
+@ObjectType()
 export default class Token {
   @Field(() => String, { nullable: false })
   token: string
+
+  @Field(() => refreshToken, { nullable: false })
+  refreshTokenExpiration: refreshToken
 
   @Field(() => String, { nullable: false })
   refreshToken: string

7320e5186270d9cbc64a9e95ec34e0780a67cbf1
victor stein <stein.hakase.vs@gmail.com>
2020-01-23 18:41:32 -0600

    Extended permissionResolver with the CRUD class

--- src/resolvers/permissionResolvers/index.ts
+++ src/resolvers/permissionResolvers/index.ts
@@ -1,10 +1,17 @@
 import { Resolver, Mutation, Args } from "type-graphql";
 import { Permission, permissionModel } from "../../models/permission";
 import createPermissionInterface from "./interfaces/createPermissionInterface";
+import baseCRUDResolver from "../globalResolvers/crudBaseResolver"
+
+const CRUDPermission = baseCRUDResolver(
+  'permission',
+  Permission,
+  permissionModel
+)
 
 @Resolver(() => Permission)
-export default class permissionResolvers {
-  
+export default class permissionResolvers extends CRUDPermission {
+
   @Mutation(() => Permission)
   async createPermission (
     @Args() { name, description }: createPermissionInterface

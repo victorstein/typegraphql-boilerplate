f51a8bb4dc90a62522db266e3b80a2944e0f3782
steinhakasevs <stein.hakase.vs@gmail.com>
2020-01-26 22:26:55 -0600

    - Removed unnecesary resolvers that are handled by the CRUDBase Resolver - Added CRUDBase resolver setup - Extended roleResolvers class with the CRUD Base resolver

--- src/resolvers/roleResolvers/index.ts
+++ src/resolvers/roleResolvers/index.ts
@@ -1,30 +1,34 @@
-import { Mutation, Resolver, Args, Query } from "type-graphql"
+import { Mutation, Resolver, Args, Authorized } from "type-graphql"
 import { Role, roleModel } from "../../models/role"
 import { ApolloError } from "apollo-server-express"
 import createRoleInterface from './interfaces/createRoleInterface'
-import byIdInterface from '../globalInterfaces/input/byIdInterface'
+import createCRUDResolver from "../globalResolvers/crudBaseResolver"
+import { createFilters } from "../../utils/reusableSnippets"
+
+// Define the prefix of the resolvers
+const resolverName = 'Role'
+
+// Create an enum based on the model indexes
+const modelIndexes = createFilters(roleModel, resolverName)
+
+// Initialize base CRUD factory
+const CRUDRole = createCRUDResolver({
+  prefix: resolverName,
+  returnType: Role,
+  model: roleModel,
+  allowedSearchCriterias: modelIndexes,
+  permissions: {
+    findById: ['read_all_roles', 'read_roles'],
+    readAll: ['read_all_roles', 'read_roles'],
+    deleteById: ['delete_roles']
+  }
+})
 
 @Resolver(() => Role)
-export default class roleResolvers {
-  @Query(() => Role)
-  async roleById (
-    @Args() { id }: byIdInterface
-  ): Promise<Role> {
-    try {
-      // Get the role
-      const role = await roleModel.findById(id)
-
-      // Return error if not founc
-      if (!role) { throw new Error('Unable to find a role with the provided id') }
-
-      // return the role if found
-      return role
-    } catch (e) {
-      throw new Error(e)
-    }
-  }
-
+export default class roleResolvers extends CRUDRole {
+  
   @Mutation(() => Role)
+  @Authorized(['create_roles'])
   createRole (
     @Args() { name, permissions }: createRoleInterface
   ): Promise<Role> {
@@ -33,27 +37,6 @@ export default class roleResolvers {
         name,
         permissions
       })
-    } catch (e) {
-      throw new ApolloError(e)
-    }
-  }
-
-  @Mutation(() => Boolean)
-  async deleteRole (
-    @Args() { id }: byIdInterface
-  ): Promise<boolean> {
-    try {
-      // Find the role to be deleted
-      const role = await roleModel.findById(id)
-
-      // If no role return error
-      if (!role) { throw new Error('The provided role is invalid') }
-
-      // If the role exists proceed to delete it
-      await role.remove()
-
-      // return true upon successful deleteion
-      return true
     } catch (e) {
       throw new ApolloError(e)
     }

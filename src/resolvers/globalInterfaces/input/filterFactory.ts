199043a0d96f4292c06982b9fd2997e4572d5b0b
victor stein <stein.hakase.vs@gmail.com>
2020-01-28 15:13:56 -0600

    devided filter factory in two functions (filter factory and sorting factory)

--- src/resolvers/globalInterfaces/input/filterFactory.ts
+++ src/resolvers/globalInterfaces/input/filterFactory.ts
@@ -1,14 +1,41 @@
-import { InputType, Field, EnumResolver } from "type-graphql";
+import { InputType, Field, EnumResolver, registerEnumType } from "type-graphql";
 import { intOrStringScalar } from "../../../utils/reusableSnippets/customScalars";
 
-export default function createDynamicFilterType(allowedSearchCriterias: any, prefix: string) {
-  @InputType(`${prefix}Filter`)
-  class Filter {
-    @Field(() => allowedSearchCriterias, { nullable: false })
-    field: EnumResolver
-  
-    @Field(() => intOrStringScalar, { nullable: false })
-    value: string | number
-  }
-  return Filter;
+enum direction {
+  ASCENDING = 1,
+  DESCENDING = -1
+}
+
+registerEnumType(direction, { name: 'direction' })
+
+export function createDynamicFilterType(
+  allowedSearchCriterias: any,
+  prefix: string
+) {
+    @InputType(`${prefix}Filter`)
+    class Filter {
+      @Field(() => allowedSearchCriterias, { nullable: false })
+      field: EnumResolver
+    
+      @Field(() => intOrStringScalar, { nullable: false })
+      value: string | number
+    }
+
+    return Filter
+}
+
+export function createDynamicSortType(
+  allowedSearchCriterias: any,
+  prefix: string
+) {
+  @InputType(`${prefix}Sort`)
+    class Sort {
+      @Field(() => allowedSearchCriterias, { nullable: false })
+      field: EnumResolver
+    
+      @Field(() => direction, { nullable: false })
+      direction: direction
+    }
+
+    return Sort
 }

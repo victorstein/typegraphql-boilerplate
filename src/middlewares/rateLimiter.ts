81e3f0416ac8f61d10aeb37e6d78484ce8dbc936
victor stein <stein.hakase.vs@gmail.com>
2020-01-29 12:56:31 -0600

    Created rate limiter middleware (decorator)

---
+++ src/middlewares/rateLimiter.ts
@@ -0,0 +1,36 @@
+import { createMethodDecorator } from "type-graphql";
+import { ipModel } from "../models/ip";
+
+export default function LimitRate(offenseName:string, offenseLimit:number) {
+  return createMethodDecorator(async ({ context }, next) => {
+    const ctx: any = context
+
+    // Get the users ip
+    const { req: { ip } } = ctx
+
+    // Locate the users offense and ip
+    const offense = await ipModel.findOne({ ip, offense: offenseName })
+
+    // If theres no offense create a new offense
+    if (!offense) {
+      await ipModel.create({ ip, offense: offenseName, times: 1 })
+      return next()
+    }
+
+    // Check if we have exceeded the offense limit
+    if (offense.times >= offenseLimit) {
+      throw new Error('Unable to process your request. Try again later')
+    }
+
+    // If theres a registered offense incremet the offense iteration
+    offense.set({
+      times: offense.times + 1
+    })
+
+    // save the offense
+    await offense.save()
+
+    // Search if the user has an offense
+    return next();
+  });
+}

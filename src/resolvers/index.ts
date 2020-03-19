0a39060277e6973fb605aa8afd537fd729efcb3f
victor stein <stein.hakase.vs@gmail.com>
2020-03-19 17:32:08 -0600

    added authorization resolvers to the exports

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,9 +1,11 @@
 import UserResolvers from './userResolvers'
 import RoleResolvers from './roleResolvers'
 import PermissionResolvers from './PermissionResolvers'
+import AuthorizationResolvers from './globalResolvers/authorizationResolvers'
 
 export default [
   UserResolvers,
   RoleResolvers,
-  PermissionResolvers
+  PermissionResolvers,
+  AuthorizationResolvers
 ]

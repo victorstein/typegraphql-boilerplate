b3519371f21033077cd2a3f0483581f44d855371
steinhakasevs <stein.hakase.vs@gmail.com>
2020-05-05 20:09:17 -0600

    created isSameAs custom validator

--- src/utils/customValidators/validators.ts
+++ src/utils/customValidators/validators.ts
@@ -130,6 +130,25 @@ export function IsBefore(validationOptions?: ValidationOptions) {
   };
 }
 
+export function IsSameAs(property: string, validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: "IsSameAs",
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+          return  value === relatedValue
+        }
+      }
+    });
+  };
+}
+
 export function IsBeforeOrSame(validationOptions?: ValidationOptions) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
